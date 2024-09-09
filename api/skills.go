bf8924c35d4bf85888110632d7e5ee921a1fa36a
Robin Nilsson <robin.ingemar.nilsson@gmail.com>
2024-09-08 18:11:41 -0600

    feat(skills): done

--- api/skills.go
+++ api/skills.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"github.com/nilssonr/agentside/skill"
 )
 
@@ -33,21 +35,77 @@ type updateSkillRequest struct {
 }
 
 func (h skillHandler) createSkill(w http.ResponseWriter, r *http.Request) {
+	var body createSkillRequest
+	if err := render.Decode(r, &body); err != nil {
+		handleError(w, r, err)
+		return
+	}
 
+	request := &skill.Skill{
+		Name:           body.Name,
+		TenantID:       tenantID(r),
+		LastModifiedBy: userID(r),
+		LastModifiedAt: time.Now(),
+	}
+
+	result, err := h.SkillService.CreateSkill(r.Context(), request)
+	if err != nil {
+		handleError(w, r, err)
+		return
+	}
+
+	render.JSON(w, r, result)
 }
 
 func (h skillHandler) getSkills(w http.ResponseWriter, r *http.Request) {
+	result, err := h.SkillService.GetSkills(r.Context(), tenantID(r))
+	if err != nil {
+		handleError(w, r, err)
+		return
+	}
 
+	render.JSON(w, r, result)
 }
 
 func (h skillHandler) getSkill(w http.ResponseWriter, r *http.Request) {
+	result, err := h.SkillService.GetSkill(r.Context(), tenantID(r), chi.URLParam(r, "skillID"))
+	if err != nil {
+		handleError(w, r, err)
+		return
+	}
 
+	render.JSON(w, r, result)
 }
 
 func (h skillHandler) updateSkill(w http.ResponseWriter, r *http.Request) {
+	var body updateSkillRequest
+	if err := render.Decode(r, &body); err != nil {
+		handleError(w, r, err)
+		return
+	}
 
+	request := &skill.Skill{
+		ID:             chi.URLParam(r, "skillID"),
+		Name:           body.Name,
+		TenantID:       tenantID(r),
+		LastModifiedBy: userID(r),
+		LastModifiedAt: time.Now(),
+	}
+
+	result, err := h.SkillService.UpdateSkill(r.Context(), request)
+	if err != nil {
+		handleError(w, r, err)
+		return
+	}
+
+	render.JSON(w, r, result)
 }
 
 func (h skillHandler) deleteSkill(w http.ResponseWriter, r *http.Request) {
+	if err := h.SkillService.DeleteSkill(r.Context(), tenantID(r), chi.URLParam(r, "skillID")); err != nil {
+		handleError(w, r, err)
+		return
+	}
 
+	render.NoContent(w, r)
 }
